cb2c28519e971cb466b8af7bfa718c7b4cdfdde4
Anshika Aggarwal <aggarwalanshika76@gmail.com>
2025-12-17 22:36:16 +0530

    Refactor database connection setup
    Removed default database URL and connect_args for SQLite.

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,13 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:anshu@2005@localhost:1089/digital_banking")
-
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args=connect_args,
     future=True
 )
 
