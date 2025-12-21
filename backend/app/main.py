f548ccb8f879abc878de95a3b5a71b0278229197
Anshika Aggarwal <aggarwalanshika76@gmail.com>
2025-12-21 22:23:49 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI, Depends
 from app.auth.router import router as auth_router
 from app.accounts.router import router as accounts_router
 from app.dependencies import get_current_user_email
+from app.transactions.router import router as transactions_router
 
 app = FastAPI(title="Modern Digital Banking API")
 
 app.include_router(auth_router)
 app.include_router(accounts_router)
+app.include_router(transactions_router)
 
 
 @app.get("/protected")
@@ -15,3 +17,4 @@ def protected_route(current_user: str = Depends(get_current_user_email)):
         "message": "You are authenticated",
         "user": current_user
     }
+
