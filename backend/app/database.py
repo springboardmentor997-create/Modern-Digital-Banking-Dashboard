5aaf46b6cea6a149ad0f9729cf653fa29637499e
devAnshikaAggarwal <aggarwalanshika76@gmail.com>
2025-12-22 17:38:45 +0530

    private/public key pair for JWT authentication

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,6 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
