b73f012d2f5618c18593bcbae9faf8de66e1d44d
Anshika Aggarwal <aggarwalanshika76@gmail.com>
2025-12-24 19:15:02 +0530

    Add Alert model to SQLAlchemy imports

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,11 +2,14 @@ from .base import Base
 from .user import User
 
 # Central place to import models for SQLAlchemy
-from app.models.user import User
 from app.accounts.models import Account
 from app.transactions.models import Transaction
-# DO NOT import Budget here
+from app.alerts.models import Alert
 
-from app.alerts.models import Alerts
-
-__all__ = ["Base", "User"]
+__all__ = [
+    "Base",
+    "User",
+    "Account",
+    "Transaction",
+    "Alert",
+]
