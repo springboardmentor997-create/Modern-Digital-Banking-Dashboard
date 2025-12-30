4f0e326be7717f24862f5b33fa0e8d5abd03f558
Anshika Aggarwal <aggarwalanshika76@gmail.com>
2025-12-30 18:51:13 +0530

    Update print statement for table creation message

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,10 +1,6 @@
 from app.database import engine
 from app.models.base import Base
 
-# IMPORTANT: import models so SQLAlchemy sees them
-import app.models
-import app.bills.models
-
-print("Creating missing tables...")
+print("Creating all tables...")
 Base.metadata.create_all(bind=engine)
 print("Done.")
