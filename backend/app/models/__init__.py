a9f199ed08ad73bbce5ce89643f9c0a8c74032c7
Anshika Aggarwal <aggarwalanshika76@gmail.com>
2025-12-30 18:50:08 +0530

    Add Reward model to central imports

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -5,6 +5,7 @@ from .user import User
 from app.accounts.models import Account
 from app.transactions.models import Transaction
 from app.alerts.models import Alert
+from app.rewards.models import Reward
 
 __all__ = [
     "Base",
