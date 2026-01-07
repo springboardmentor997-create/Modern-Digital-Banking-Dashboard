fad244d2dec59a9a9ff6fa460975fc3b52ab3f84
krishnapothuru30-commits <krishnapothuru30@gmail.com>
2026-01-07 22:38:41 +0530

    Updated the backend

--- crud.py
+++ crud.py
@@ -2,16 +2,17 @@ from sqlalchemy.orm import Session
 from app import models
 from app.auth import get_password_hash
 
+
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
+
 def create_user(db: Session, user_in):
-    hashed = get_password_hash(user_in.password)
     user = models.User(
         name=user_in.name,
+        phone=user_in.phone,
         email=user_in.email,
-        password=hashed,
-        phone=user_in.phone
+        hashed_password=get_password_hash(user_in.password)
     )
     db.add(user)
     db.commit()
